d7fcf9cbd6413b2d6112c1d23a00dbd99fe5a1f8
Dennis Haegler <dennis.haegler@kaeuferportal.de>
2017-11-08 23:08:52 +0100

    Use vim-plug

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cp -r ~/.vim ~/.vim.old 2>/dev/null
-rm -fr ~/.vim 2>/dev/null
-rm -f ~/.vimrc 2>/dev/null
-git clone git@github.com:DenniJensen/vimfiles ~/.vim && cd ~/.vim && vim -c BundleInstall! -c q -c q -u bundles.vim && ln -s ~/.vim/vimrc ~/.vimrc
--- update_bundles.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-vim -c BundleInstall! -c q -c q -u bundles.vim
